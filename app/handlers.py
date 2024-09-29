4d6fff47e0993a3c654e1613f41ea7256ef78b3a
Nick <marnsd2403@gmail.com>
2024-09-30 00:11:54 +0300

    schedule-for-2-groups

--- app/handlers.py
+++ app/handlers.py
@@ -1,8 +1,13 @@
 from aiogram import F, Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart, Command
-from app.keyboards import main
-
+from app.keyboards import (main, first_group, second_group,)
+from .schedule import (FIRST_GROUP_MN, FIRST_GROUP_TU,
+                        FIRST_GROUP_WD, FIRST_GROUP_TH,
+                        FIRST_GROUP_FR, FIRST_GROUP_SD,
+                        SECOND_GROUP_MN, SECOND_GROUP_TU,
+                        SECOND_GROUP_WD, SECOND_GROUP_TH,
+                        SECOND_GROUP_FR, SECOND_GROUP_SD,)
 
 router = Router()
 
@@ -15,11 +20,71 @@ async def cmd_start(message: Message):
 @router.message(F.text == "1 группа")
 async def cmd_start(message: Message):
     await message.answer("Распеса 1 группы",
+                         reply_markup=first_group)
+
+@router.message(F.text == "Понедельник_1")
+async def cmd_start(message: Message):
+    await message.answer(FIRST_GROUP_MN,
+                         reply_markup=main)
+
+@router.message(F.text == "Вторник_1")
+async def cmd_start(message: Message):
+    await message.answer(FIRST_GROUP_TU,
+                         reply_markup=main)
+
+@router.message(F.text == "Среда_1")
+async def cmd_start(message: Message):
+    await message.answer(FIRST_GROUP_TH,
+                         reply_markup=main)
+
+@router.message(F.text == "Четверг_1")
+async def cmd_start(message: Message):
+    await message.answer(FIRST_GROUP_WD,
+                         reply_markup=main)
+
+@router.message(F.text == "Пятница_1")
+async def cmd_start(message: Message):
+    await message.answer(FIRST_GROUP_FR,
+                         reply_markup=main)
+
+@router.message(F.text == "Суббота_1")
+async def cmd_start(message: Message):
+    await message.answer(FIRST_GROUP_SD,
                          reply_markup=main)
 
 @router.message(F.text == "2 группа")
 async def cmd_start(message: Message):
     await message.answer("Распеса 2 группы",
+                         reply_markup=second_group)
+
+@router.message(F.text == "Понедельник_2")
+async def cmd_start(message: Message):
+    await message.answer(SECOND_GROUP_MN,
+                         reply_markup=main)
+
+@router.message(F.text == "Вторник_2")
+async def cmd_start(message: Message):
+    await message.answer(SECOND_GROUP_TU,
+                         reply_markup=main)
+
+@router.message(F.text == "Среда_2")
+async def cmd_start(message: Message):
+    await message.answer(SECOND_GROUP_WD,
+                         reply_markup=main)
+
+@router.message(F.text == "Четверг_2")
+async def cmd_start(message: Message):
+    await message.answer(SECOND_GROUP_TH,
+                         reply_markup=main)
+
+@router.message(F.text == "Пятница_2")
+async def cmd_start(message: Message):
+    await message.answer(SECOND_GROUP_FR,
+                         reply_markup=main)
+
+@router.message(F.text == "Суббота_2")
+async def cmd_start(message: Message):
+    await message.answer(SECOND_GROUP_SD,
                          reply_markup=main)
 
 @router.message(F.text == "3 группа")
